0627d2ef0e52b0d60c2f3bdb91a820f5003472bf
rendizi <rendizi_off@gmail.com>
2024-07-17 21:36:28 +0500

    button

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -165,30 +165,22 @@ const Settings = () => {
         <div className="bg-base-200 min-h-screen">
             <Navbar />
             <div className='w-full flex flex-col justify-center items-center mt-4'>
-            <div className='w-2/3 flex gap-2'>
-    <label className="input input-bordered flex items-center gap-2 grow">
-        <input
-            type="text"
-            className="grow"
-            placeholder="username"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyPress={handleKeyPress}
-            disabled={connected}
-        />
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 16 16"
-            fill="currentColor"
-            className="h-4 w-4 opacity-70">
-            <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd" />
-        </svg>
-    </label>
-    <button className='btn btn-accent'>Search</button>
-</div>
+            <div className='flex gap-2'>
+                <div className='join'>
+                    <label className="input input-bordered flex items-center gap-2 grow join-item">
+                        <input
+                            type="text"
+                            className="grow"
+                            placeholder="username"
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                            onKeyPress={handleKeyPress}
+                            disabled={connected}
+                        />
+                    </label>
+                    <button className='btn btn-accent join-item'>Search</button>
+                </div>
+            </div>
 
                 {response ?
                  <div className='mt-4'>
